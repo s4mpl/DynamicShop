47dccaa3be81928d38163d58983a7fefe11d2fbe
s4mpl <brandanjroachell@att.net>
2021-12-15 00:44:28 -0500

    Updated format of price changes commands.

--- src/me/tareqalyousef/dynamicshop/commands/LowestCommand.java
+++ src/me/tareqalyousef/dynamicshop/commands/LowestCommand.java
@@ -42,28 +42,38 @@ public class LowestCommand implements CommandExecutor {
             return false;
         }
 
-        if(type) {
-            HashMap<String, Double[]> prices = Util.getItemPricesAndChanges();
+        HashMap<String, Double[]> prices = Util.getItemPricesAndChanges();
 
-            // Create a list from elements of HashMap
-            List<Map.Entry<String, Double[]> > list = new LinkedList<Map.Entry<String, Double[]>>(prices.entrySet());
-            // Sort the list
-            Collections.sort(list, new Comparator <Map.Entry<String, Double[]>>() {
+        // Create a list from elements of HashMap
+        List<Map.Entry<String, Double[]> > list = new LinkedList<Map.Entry<String, Double[]>>(prices.entrySet());
+        // Sort the list
+        if(type) {
+            Collections.sort(list, new Comparator<Map.Entry<String, Double[]>>() {
                 public int compare(Map.Entry<String, Double[]> o1, Map.Entry<String, Double[]> o2) {
                     return o1.getValue()[0].compareTo(o2.getValue()[0]);
                 }
             });
-            prices = list.stream().collect(Collectors.toMap(entry -> entry.getKey(), entry -> entry.getValue(),
-                    (entry1, entry2) -> entry2, LinkedHashMap::new));
 
             player.sendMessage(Settings.PREFIX_COLOR + plugin.getConfig().getString("prefix") + Settings.DEFAULT_COLOR + " Lowest Item Prices");
+        } else {
+            Collections.sort(list, new Comparator<Map.Entry<String, Double[]>>() {
+                public int compare(Map.Entry<String, Double[]> o1, Map.Entry<String, Double[]> o2) {
+                    return o1.getValue()[1].compareTo(o2.getValue()[1]);
+                }
+            });
 
+            player.sendMessage(Settings.PREFIX_COLOR + plugin.getConfig().getString("prefix") + Settings.DEFAULT_COLOR + " Lowest Item Price Changes");
+        }
+        prices = list.stream().collect(Collectors.toMap(entry -> entry.getKey(), entry -> entry.getValue(),
+                (entry1, entry2) -> entry2, LinkedHashMap::new));
+
+        if (type) {
             for (String item : prices.keySet()) {
-                ChatColor color = prices.get(item)[1] >= 1 ? ChatColor.GREEN : ChatColor.RED;
+                ChatColor color = prices.get(item)[1] >= 1 ? ChatColor.DARK_GREEN : ChatColor.RED;
                 String sign = prices.get(item)[1] >= 1 ? "+" : "-";
 
                 player.sendMessage(Settings.HIGHLIGHT_COLOR + String.valueOf(place) + ") " + Settings.DEFAULT_COLOR + item.toLowerCase() + " " +
-                        Settings.MONEY_COLOR + "$" + String.format("%.2f%%", prices.get(item)[0]) + color + " (" + sign +
+                        Settings.MONEY_COLOR + "$" + String.format("%.2f", prices.get(item)[0]) + color + " (" + sign +
                         String.format("%.2f%%", 100 * Math.abs(1 - prices.get(item)[1])) + ")");
 
                 if (place == 9)
@@ -72,22 +82,13 @@ public class LowestCommand implements CommandExecutor {
                 place++;
             }
         } else {
-            HashMap<String, Double> changes = Util.getItemPriceChanges();
+            for (String item : prices.keySet()) {
+                ChatColor color = prices.get(item)[1] >= 1 ? ChatColor.DARK_GREEN : ChatColor.RED;
+                String sign = prices.get(item)[1] >= 1 ? "+" : "-";
 
-            changes = changes.entrySet()
-                    .stream()
-                    .sorted(Map.Entry.comparingByValue())
-                    .collect(Collectors.toMap(entry -> entry.getKey(), entry -> entry.getValue(),
-                            (entry1, entry2) -> entry2, LinkedHashMap::new));
-
-            player.sendMessage(Settings.PREFIX_COLOR + plugin.getConfig().getString("prefix") + Settings.DEFAULT_COLOR + " Lowest Item Percentage Changes");
-
-            for (String item : changes.keySet()) {
-                ChatColor color = changes.get(item) >= 1 ? ChatColor.GREEN : ChatColor.RED;
-                String sign = changes.get(item) >= 1 ? "+" : "-";
-
-                player.sendMessage(Settings.HIGHLIGHT_COLOR + String.valueOf(place) + ") " + Settings.DEFAULT_COLOR + item.toLowerCase() + " " + color + "(" + sign +
-                        String.format("%.2f%%", 100 * Math.abs(1 - changes.get(item))) + ")");
+                player.sendMessage(Settings.HIGHLIGHT_COLOR + String.valueOf(place) + ") " + Settings.DEFAULT_COLOR + item.toLowerCase() + " " +
+                        color + sign + String.format("%.2f%%", 100 * Math.abs(1 - prices.get(item)[1])) + Settings.DEFAULT_COLOR + " (" +
+                        Settings.MONEY_COLOR + "$" + String.format("%.2f", prices.get(item)[0]) + Settings.DEFAULT_COLOR + ")");
 
                 if (place == 9)
                     break;
